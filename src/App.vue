0e246c33dc1f9626576d07cad4521f8f9e01f51c
Wyatt Nolen <wyattnolen@gmail.com>
2021-06-01 00:19:10 -0400

    Basic Logic

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
+<template>  
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>{{pokemon.name}}</h2>
+  <div
+    class="pokemon-image"
+    :class="hidden ? 'pokemon-image--hidden' : '' "
+    :style="'background-image: url(' + pokemon.imageUrl + ')'">
+  </div>
+  <div>
+    <input v-model="userGuess">
+    <button @click="checkGuess">Submit</button>
+  </div>
+
 </template>
 
 <script>
+import axios from 'axios';
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      pokemon: {
+        name: '',
+        imageUrl: ''
+      },
+      userGuess: '',
+      hidden: true,
+    };
+  },
+  mounted () {
+    axios
+    /* Get Pokemon Data */
+      .get('https://pokeapi.co/api/v2/pokemon/1')
+      .then(response => {
+        this.pokemon.name = response.data.name;
+        this.pokemon.imageUrl = response.data.sprites.front_default
+        })
+      
+  },
+  computed: {
+  },
+  methods: {
+    normalizeUserGuess() {
+      return this.userGuess.toLowerCase().trim();
+    },
+
+    checkGuess() {
+      if (this.normalizeUserGuess() === this.pokemon.name) {
+        this.hidden = false;
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .pokemon-image {
+    min-width: 300px;
+    min-height: 300px;
+    background-repeat: no-repeat;
+    background-size: contain;
+  }
+  .pokemon-image--hidden {
+    /* filter: sepia() saturate(10000%) hue-rotate(180deg); */
+    filter: brightness(0%);
+  }
 </style>
